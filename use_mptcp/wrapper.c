7fd3b1cd3b76c6f55ccbb853d6e4e1ac6f9db675
Paolo Abeni <pabeni@redhat.com>
2019-12-18 15:59:47 +0100

    use_mptcp: cover more socket() usage

--- use_mptcp/wrapper.c
+++ use_mptcp/wrapper.c
@@ -9,14 +9,19 @@
 
 int socket(int family, int type, int protocol)
 {
+	/* the 'type' field may encode socket flags */
 	if ((family != AF_INET && family != AF_INET6) ||
-	    type != SOCK_STREAM || protocol != IPPROTO_TCP)
+	    (type & 0xff)  != SOCK_STREAM)
+		goto do_socket;
+
+	/* socket(AF_INET, SOCK_STREM, 0 maps to TCP, too */
+	if (protocol && protocol != IPPROTO_TCP)
 		goto do_socket;
 
 	if (getenv("USE_MPTCP_DEBUG"))
 		fprintf(stderr, "use_mptcp: changing socket proto from %d to %d\n",
-			protocol, protocol + 256);
-	protocol += 256;
+			protocol, IPPROTO_TCP + 256);
+	protocol = IPPROTO_TCP + 256;
 
 do_socket:
 	return syscall(__NR_socket, family, type, protocol);
