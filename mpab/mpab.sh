de56e738afae0e8a6630d819fb3b3b2a5d9d3b56
Paolo Abeni <pabeni@redhat.com>
2020-06-15 16:00:49 +0200

    mbap: add support for multiple test mode
    -a <num> specify the number of addresses announced by the server
    and -s <num> specify the number of subflow created by the client
    using local addresses.

--- mpab/mpab.sh
+++ mpab/mpab.sh
@@ -10,13 +10,18 @@ FULLNAME=`readlink -f $0`
 WRAPPER=`dirname $FULLNAME`/../use_mptcp/use_mptcp.sh
 WRAPPER=`readlink -f $WRAPPER`
 TOOL=ab
+ANNUNCE=1
+SUBFLOWS=0
 
 help()
 {
-	echo -e "syntax:\n$0 [-d] [-c] [-n <name>] [-h]\n"\
-		"\t-d\t debug this script\n" \
+	echo -e "syntax:\n$0 [-a <n>] [-c] [-d] [-f] [-s <num> ] [-w] [-h]\n"\
+		"\t-a\t the server annunces <n> addresses to the client (default: 1)\n" \
 		"\t-c\t capture traffic into pcap.dump\n" \
+		"\t-d\t debug this script\n" \
 		"\t-f\t \"fast\" mode: do not log access\n" \
+		"\t-s\t the sever will use <num> local accresses (default: 0) \n" \
+		"\t-w\t use weighttp client (default: ab)\n" \
 		"\t-h\t show this help\n"
 	exit 0
 }
@@ -46,13 +51,22 @@ init()
 		ip -n $NS link set dev $DEV up
 		ID=$((ID + 1))
 		ip -n $NS addr add dev $DEV 192.168.1.$ID/24
-		ip -n $NS addr add dev $DEV 192.168.2.$ID/24
+
+		for ADDR in `seq 2 $((ACCOUNCE + 1))`; do
+			ip -n $NS addr add dev $DEV 192.168.$ADDR.$ID/24
+		done
 	done
 
-	ip -n $NS_SRV mptcp endpoint add 192.168.2.1 dev eth0 signal
-	ip -n $NS_SRV mptcp limits set subflows 2
-	ip -n $NS_CL mptcp limits set subflows 2
-	ip -n $NS_CL mptcp limits set add_addr_accepted 2
+	for ADDR in `seq 2 $((ACCOUNCE + 1))`; do
+		ip -n $NS_SRV mptcp endpoint add 192.168.$ADDR.1 dev eth0 signal
+	done
+	for ADDR in `seq 3 $((SUBFLOWS + 2))`; do
+		ip -n $NS_CL addr add dev eth1 192.168.1.$ADDR/24
+		ip -n $NS_CL mptcp endpoint add 192.168.1.$ADDR dev eth1 subflow
+	done
+	ip -n $NS_SRV mptcp limits set subflows $((ACCOUNCE + $SUBFLOWS))
+	ip -n $NS_CL mptcp limits set subflows $((ACCOUNCE + $SUBFLOWS))
+	ip -n $NS_CL mptcp limits set add_addr_accepted $ACCOUNCE
 
 	dd if=/dev/null of=$WD/1KB bs=1 count=1024
 	dd if=/dev/null of=$WD/50KB bs=50 count=1024
@@ -94,8 +108,11 @@ ENDL
 	ip netns exec $NS_CL $WRAPPER $TOOL -c 100 -n 100000 192.168.1.1/300KB
 }
 
-while getopts "cdfwh" option; do
+while getopts "a:cdfs:wh" option; do
 	case $option in
+		a)
+			ANNOUNCE=$OPTARG
+			;;
 		c)
 			DUMP=1
 			;;
@@ -104,6 +121,9 @@ while getopts "cdfwh" option; do
 			;;
 		f)
 			FAST=1
+			;;
+		s)
+			SUBFLOWS=$OPTARG
 			;;
 		w)
 			TOOL="weighttp -t "$(nproc)
